86f3f51e55271139e879b16404e99255f8fe00cd
Tommaso Peduzzi <tommaso.peduzzi@gmx.ch>
2022-07-20 13:40:13 +0200

    FPGA: Added control lines to control entity and connected all control lines

--- fpga/hdl/control.vhd
+++ fpga/hdl/control.vhd
@@ -27,7 +27,42 @@ port (
     latch_op2 : IN std_logic;
     latch_lsb_imm : IN std_logic;
     latch_msb_imm : IN std_logic; 
-    control_lines : OUT std_logic_vector(35 downto 0)  -- example
+
+    -- control lines
+    control_latch_opcode_op1 : OUT std_logic; -- latch opcode op1
+    control_latch_op2 : OUT std_logic; -- latch opcode op2
+    control_latch_lsb_imm : OUT std_logic; -- latch lsb immediate
+    control_latch_msb_imm : OUT std_logic; -- latch msb immediate
+
+    mem_we : OUT std_logic; -- write enable memory
+
+    pc_inc : OUT std_logic; -- increment PC
+    pc_load : OUT std_logic; -- load PC from memory bus
+
+    sp_inc : OUT std_logic; -- increment SP
+    sp_decr : OUT std_logic; -- decrement SP
+
+    alu_op : OUT std_logic_vector(3 downto 0); -- ALU operation
+
+    reg_we : OUT std_logic_vector(2 downto 0); -- write enable registers
+    reg_we_l : OUT std_logic_vector(2 downto 0); -- write enable LSB registers
+    reg_we_m : OUT std_logic_vector(2 downto 0); -- write enable MSB registers
+
+    -- main bus
+    main_alu_sel : OUT std_logic; -- ALU select
+    main_mem_sel : OUT std_logic; -- memory select
+    main_reg_sel : OUT std_logic_vector(2 downto 0); -- register select
+    main_reg_l_sel : OUT std_logic_vector(2 downto 0); -- register LSB select
+    main_reg_m_sel : OUT std_logic_vector(2 downto 0); -- register MSB select
+
+    -- address bus
+    addr_pc_sel : OUT std_logic; -- PC select
+    addr_sp_sel : OUT std_logic; -- SP select
+    addr_reg_sel : OUT std_logic_vector(2 downto 0); -- register select
+
+    -- alu operand bus
+    alu_rhs_sel : OUT std_logic_vector(2 downto 0); -- rhs select
+    alu_lhs_sel : OUT std_logic_vector(2 downto 0)  -- lhs select
 );
 end control;
 architecture architecture_control of control is
