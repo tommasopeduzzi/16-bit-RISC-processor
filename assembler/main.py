fdeba1d0aa30dcb3f5fb2a0f96bf07a569357066
Tommaso Peduzzi <tommaso.peduzzi@gmx.ch>
2022-07-27 17:42:56 +0200

    Assembler: Add option to extend image to 64k with 0s

--- assembler/main.py
+++ assembler/main.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="Input file")
     parser.add_argument("output", help="Output file")
+    parser.add_argument("--extend_binary", help="Extends the binary file to 64K", action="store_true")
     args = parser.parse_args()
     assembler = Assembler()
     assembler.assemble(args.input)
@@ -12,3 +13,5 @@ if __name__ == "__main__":
         for i, byte in enumerate(assembler.binary):
             print(i, bin(byte)[2:].zfill(8))
             f.write(bytes([byte]))
+        if args.extend_binary:
+            f.write(bytes([0] * (65536 - len(assembler.binary))))
