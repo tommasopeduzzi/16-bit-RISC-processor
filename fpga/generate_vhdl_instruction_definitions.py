99b8c282466b04a5cefa373536e67243832c5949
Tommaso Peduzzi <tommaso.peduzzi@gmx.ch>
2022-08-24 15:20:26 +0200

    FPGA: Add program to generate instruction opcode constants

---
+++ fpga/generate_vhdl_instruction_definitions.py
@@ -0,0 +1,28 @@
+files = ["instructions/core.instr"]
+
+vhdl_file = open("fpga/instructions.vhd", "w")
+number_of_instructions = 0
+
+vhdl_file.write("package instructions is\n")
+
+def write_vhdl_constant(mnemonic: str, opcode: int):
+    vhdl_file.write(f"\tconstant {mnemonic} : std_logic_vector := \"{bin(opcode)[2:].zfill(6)}\";\n")
+
+for file in files:
+    with open(file, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            if line.startswith("#"):
+                continue
+            parts = line.split()
+            if parts[0] == "nop":
+                write_vhdl_constant("nop", 0b000000)
+                continue
+            elif parts[0] == "halt":
+                write_vhdl_constant("halt", 0b111111)
+                continue
+            mnemonic = "_".join(parts)
+            number_of_instructions += 1
+            write_vhdl_constant(mnemonic, number_of_instructions)
+vhdl_file.write("end package instructions;")
+vhdl_file.close()
