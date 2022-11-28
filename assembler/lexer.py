5ee1281110bcbc99f1e85ece6ca8e2d3b880e318
Tommaso Peduzzi <tommaso.peduzzi@gmx.ch>
2022-11-28 14:52:27 +0100

    Assembler: Rewrite Lexer and Parser to support binary expressions as immediates
    Has operator precendence and parens, but probably full of bugs.

--- assembler/lexer.py
+++ assembler/lexer.py
@@ -4,6 +4,7 @@ from typing import List
 
 from dataclasses import dataclass
 
+
 class TokenType(Enum):
     LABEL = 0
     WORD = 1
@@ -19,6 +20,12 @@ class TokenType(Enum):
     INCLUDE = 11
     STRING = 12
     INTERRUPT = 13
+    OPERATOR = 14
+    LPAREN = 15
+    RPAREN = 16
+    COMMA = 17
+    EOF = 18
+
 
 @dataclass
 class Token:
@@ -27,9 +34,33 @@ class Token:
 
 
 class Lexer:
+    operators = ["+", "-", "*", "/"]
+
+    keywords = {
+        "macro": TokenType.START_MACRO,
+        "endmacro": TokenType.END_MACRO,
+        "dev": TokenType.MACRO_ARGUMENT_TYPE,
+        "imm": TokenType.MACRO_ARGUMENT_TYPE,
+        "addr": TokenType.MACRO_ARGUMENT_TYPE,
+        # TODO: Add device to macro arguments (how tf did I miss this??)
+        "data": TokenType.DATA,
+        "include": TokenType.INCLUDE,
+        "interrupt": TokenType.INTERRUPT,
+    }
+
+    special_characters = {
+        "(": TokenType.LPAREN,
+        ")": TokenType.RPAREN,
+        "$": TokenType.REGISTER,
+        "?": TokenType.DEVICE,
+        "@": TokenType.ADDRESS,
+        "%": TokenType.MACRO_ARGUMENT,
+        ",": TokenType.COMMA,
+    }
+
     def __init__(self) -> None:
         pass
-    
+
     def split_line(line: str) -> List[str]:
         items: List[str] = [""]
         currently_string = False
@@ -48,44 +79,59 @@ class Lexer:
                 items[-1] += char
         return [item.strip() for item in items if not item == ""]
 
+    def append_token(self, token: Token) -> str:
+        self.tokens.append(token)
+        return ""
+
+    def append_word(self, word: str) -> str:
+        if word in self.keywords:
+            self.append_token(Token(self.keywords[word], word))
+        elif re.match(r'[a-zA-Z]+:', word):
+            self.tokens.append(Token(TokenType.LABEL, word))
+        elif re.match(r'[a-zA-Z]+', word):
+            self.tokens.append(Token(TokenType.WORD, word))
+        elif re.match(r'0x[0-9a-fA-F]+', word):
+            self.tokens.append(
+                Token(TokenType.IMMEDIATE, word))
+        elif re.match(r'0b[01]+', word):
+            self.tokens.append(
+                Token(TokenType.IMMEDIATE, word))
+        elif re.match(r'[0-9]+', word):
+            self.tokens.append(Token(TokenType.IMMEDIATE, word))
+        return ""
 
     def lex(self, contents: str) -> List[Token]:
         self.tokens = []
-        for line in contents.split('\n'):
-            words = Lexer.split_line(line)
-            for word in words:
-                if word.startswith(';'):
-                    break
-                elif word == "macro":
-                    self.tokens.append(Token(TokenType.START_MACRO, word))
-                elif word == "endmacro":
-                    self.tokens.append(Token(TokenType.END_MACRO, word))
-                elif ["imm", "addr", "reg"].count(word) == 1:
-                    self.tokens.append(Token(TokenType.MACRO_ARGUMENT_TYPE, word))
-                elif word == "data":
-                    self.tokens.append(Token(TokenType.DATA, word))
-                elif word == "include":
-                    self.tokens.append(Token(TokenType.INCLUDE, word))
-                elif word == "interrupt": 
-                    self.tokens.append(Token(TokenType.INTERRUPT, word))
-                elif re.match(r'[a-zA-Z]+:', word):
-                    self.tokens.append(Token(TokenType.LABEL, word))
-                elif re.match(r'[a-zA-Z]+', word):
-                    self.tokens.append(Token(TokenType.WORD, word))
-                elif re.match(r'-*0x[0-9a-fA-F]+', word):
-                    self.tokens.append(Token(TokenType.IMMEDIATE, word))
-                elif re.match(r'-*0b[01]+', word):
-                    self.tokens.append(Token(TokenType.IMMEDIATE, word))
-                elif re.match(r'-*[0-9]+', word):
-                    self.tokens.append(Token(TokenType.IMMEDIATE, word))
-                elif word.startswith('$'):
-                    self.tokens.append(Token(TokenType.REGISTER, word))
-                elif word.startswith('?'):
-                    self.tokens.append(Token(TokenType.DEVICE, word))
-                elif word.startswith('@'):
-                    self.tokens.append(Token(TokenType.ADDRESS, word))
-                elif word.startswith('%'):
-                    self.tokens.append(Token(TokenType.MACRO_ARGUMENT, word))
-                elif word.startswith('"'):
-                    self.tokens.append(Token(TokenType.STRING, word[1:-1]))
+        contents = list(contents)
+        word: str = ""
+        while len(contents) > 0:
+            char = contents.pop(0)
+            if char.isspace():
+                word = self.append_word(word)
+                continue
+            elif char in self.operators:
+                word = self.append_word(word)
+                self.tokens.append(Token(TokenType.OPERATOR, char))
+                continue
+            elif char in self.special_characters:
+                word = self.append_word(word)
+                self.tokens.append(Token(self.special_characters[char], char))
+                continue
+            elif char == "\"":
+                word = self.append_word(word)
+                string = ""
+                while len(contents) > 0:
+                    char = contents.pop(0)
+                    if char == "\"":
+                        break
+                    string += char
+                self.tokens.append(Token(TokenType.STRING, string))
+                continue
+            elif char == ";":
+                word = self.append_word(word)
+                while len(contents) > 0 and not char == "\n":
+                    char = contents.pop(0)
+            word += char
+            if len(contents) == 0:
+                self.append_word(word)
         return self.tokens
--- assembler/parser.py
+++ assembler/parser.py
@@ -5,13 +5,16 @@ from typing import List
 from dataclasses import dataclass
 from lexer import Lexer, Token, TokenType
 
+
 class OperandType():
     valid_operands = [
-        TokenType.IMMEDIATE, 
-        TokenType.ADDRESS, 
-        TokenType.REGISTER, 
-        TokenType.MACRO_ARGUMENT, 
+        TokenType.IMMEDIATE,
+        TokenType.ADDRESS,
+        TokenType.REGISTER,
+        TokenType.MACRO_ARGUMENT,
         TokenType.DEVICE,
+        TokenType.OPERATOR,
+        TokenType.LPAREN
     ]
     REGISTER = 0
     IMMEDIATE = 1
@@ -20,6 +23,7 @@ class OperandType():
     DEVICE = 4
     LABEL = 5
     STRING = 6
+
     def to_string(type):
         match type:
             case OperandType.REGISTER:
@@ -33,23 +37,28 @@ class OperandType():
             case _:
                 return ""
 
+
 @dataclass
 class Operand():
     type: OperandType
     value: int
+
 
 @dataclass
 class Instruction():
     mnemonic: str
     operands: List[Operand]
 
+
 @dataclass
 class Label():
     name: str
 
+
 @dataclass
 class Interrupt():
     number: int
+
 
 @dataclass
 class Macro():
@@ -57,93 +66,128 @@ class Macro():
     argument_types: List[OperandType]
     instructions: List[Instruction]
 
+
 @dataclass
 class DataItem():
     type: OperandType
     value: int
 
+
 @dataclass
 class Data():
     operands: List[DataItem]
+
+
+def precedence(operator):
+    try:
+        return {
+            "+": 1,
+            "-": 1,
+            "*": 2
+        }[operator]
+    except KeyError:
+        return -1
+
 
 class Parser:
     def __init__(self):
         self.lexer = Lexer()
         self.tokens = []
-        
+
+    def current_token(self):
+        return Token(TokenType.EOF, "") if not len(self.tokens) > 0 else self.tokens[0]
+
+    def consume_token(self):
+        return self.tokens.pop(0)
+
     def parse_file(self, file):
         with open(file, 'r') as f:
             return self.parse(f.read())
-    
-    def parse_immediate(self, value: str) -> int:
-        coefficent = 1
-        if value.startswith("-"):
-            coefficent = -1
-            value = value[1:]
+
+    def parse_immediate(self) -> int:
+        value = self.current_token().value
+        if value == "(":
+            return self.parse_expression()
+        self.consume_token()
         if re.match(r'0x[0-9a-fA-F]+', value):
-            return int(value[2:], 16) * coefficent
+            return int(value[2:], 16)
         elif re.match(r'0b[01]+', value):
-            return int(value[2:], 2) * coefficent
+            return int(value[2:], 2)
         elif re.match(r'[0-9]+', value):
-            return int(value) * coefficent
+            return int(value)
         else:
             raise Exception("Invalid number: {}".format(value))
 
-    def parse_operand(self, token: Token, macro_definition = False) -> Operand:
+    def parse_operand(self, macro_definition=False) -> Operand:
+        token = self.current_token()
         match token.type:
             case TokenType.REGISTER:
-                register = self.parse_immediate(token.value[1:])
+                token = self.consume_token()
+                register = self.parse_expression()
                 if register < 0 or register > 7:
                     raise Exception("Invalid register: {}".format(token.value))
                 return Operand(OperandType.REGISTER, register)
             case TokenType.DEVICE:
-                device = self.parse_immediate(token.value[1:])
+                token = self.consume_token()
+                device = self.parse_expression()
                 if device < 0 or device > 7:
                     raise Exception("Invalid device: {}".format(token.value))
                 return Operand(OperandType.DEVICE, device)
             case TokenType.ADDRESS:
-                if re.match(r'[a-zA-Z]+', token.value[1:]):
-                    return Operand(OperandType.LABEL, token.value[1:])
+                self.consume_token()
+                token = self.current_token()
+                if token.type == TokenType.WORD:
+                    self.consume_token()
+                    return Operand(OperandType.LABEL, token.value)
                 else:
-                    address = self.parse_immediate(token.value[1:])
+                    address = self.parse_expression()
                 if address < 0 or address > 0xFFFF:
                     raise Exception("Invalid address: {}".format(address))
                 return Operand(OperandType.ADDRESS, address)
             case TokenType.MACRO_ARGUMENT:
                 if not macro_definition:
-                    raise Exception("Macro argument found outside macro definition: {}".format(token.value))
-                return Operand(OperandType.MACRO_ARGUMENT, self.parse_immediate(token.value[1:]))
-            case TokenType.IMMEDIATE:
-                immediate = self.parse_immediate(token.value)
+                    raise Exception(
+                        "Macro argument found outside macro definition: {}".format(token.value))
+                token = self.consume_token()
+                return Operand(OperandType.MACRO_ARGUMENT, self.parse_expression())
+            case TokenType.STRING:
+                token = self.consume_token()
+                return Operand(OperandType.STRING, token.value)
+            case TokenType.IMMEDIATE | TokenType.OPERATOR | TokenType.LPAREN:
+                immediate = self.parse_expression()
                 # check if number is in range of signed 16 bit integer
                 if immediate < -0x8000 or immediate > 0x7FFF:
                     raise Exception("Invalid immediate: {}".format(immediate))
                 return Operand(OperandType.IMMEDIATE, immediate)
 
-    def parse_instruction(self, macro_definition = False) -> Instruction:
-        name = self.tokens[0].value
+    def parse_instruction(self, macro_definition=False) -> Instruction:
+        name = self.consume_token().value
         operands = []
-        self.tokens.pop(0)
-        while len(self.tokens) > 0 and OperandType.valid_operands.count(self.tokens[0].type):
-            operands.append(self.parse_operand(self.tokens[0], macro_definition))
-            self.tokens.pop(0)
+        while len(self.tokens) > 0 and self.current_token().type in OperandType.valid_operands:
+            operands.append(self.parse_operand(macro_definition))
+            if self.current_token().type == TokenType.COMMA:
+                self.consume_token()
+            else: 
+                break
         return Instruction(name, operands)
 
     def parse_macro(self) -> Macro:
-        self.tokens.pop(0)
-        if not self.tokens[0].type == TokenType.WORD:
-            raise Exception("Expected name after macro declaration, found {}".format(self.tokens[0].value))
-        macro_name = self.tokens[0].value
+        self.consume_token()
+        if not self.current_token().type == TokenType.WORD:
+            raise Exception("Expected name after macro declaration, found {}".format(
+                self.current_token().value))
+        macro_name = self.current_token().value
         argument_types = []
         instructions = []
-        self.tokens.pop(0)
-        if self.tokens[0].type == TokenType.IMMEDIATE:
-            num_args = self.parse_immediate(self.tokens[0].value)
-            self.tokens.pop(0)
+        self.consume_token()
+        try:
+            num_args = self.parse_expression()
+            self.consume_token()
             for _ in range(num_args):
-                if not self.tokens[0].type == TokenType.MACRO_ARGUMENT_TYPE:
-                    raise Exception("Expected macro argument type, found {}".format(self.tokens[0].value))
-                match self.tokens[0].value:
+                if not self.current_token().type == TokenType.MACRO_ARGUMENT_TYPE:
+                    raise Exception("Expected macro argument type, found {}".format(
+                        self.current_token().value))
+                match self.current_token().value:
                     case "imm":
                         argument_types.append(OperandType.IMMEDIATE)
                     case "addr":
@@ -151,58 +195,93 @@ class Parser:
                     case "reg":
                         argument_types.append(OperandType.REGISTER)
                     case _:
-                        raise Exception("Invalid macro argument type: {}".format(self.tokens[0].value))
-                self.tokens.pop(0)
-        else: 
-            raise Exception("Expected number of macro arguments, found {}".format(self.tokens[0].value))
-        while self.tokens[0].type != TokenType.END_MACRO:
+                        raise Exception("Invalid macro argument type: {}".format(
+                            self.current_token().value))
+                self.consume_token()
+        except Exception:
+            raise Exception("Expected number of macro arguments, found {}".format(
+                self.current_token().value))
+        while self.current_token().type != TokenType.END_MACRO:
             instruction = self.parse_instruction(macro_definition=True)
             instructions.append(instruction)
-        self.tokens.pop(0)
+        self.consume_token()
         return Macro(macro_name, argument_types, instructions)
 
     def parse_data(self) -> Data:
-        self.tokens.pop(0)
+        self.consume_token()
         operands = []
         while len(self.tokens) > 0:
-            if self.tokens[0].type == TokenType.IMMEDIATE:
-                operands.append(DataItem(OperandType.IMMEDIATE, self.parse_immediate(self.tokens[0].value)))
-            elif self.tokens[0].type == TokenType.ADDRESS:
-                try:
-                    operands.append(DataItem(OperandType.ADDRESS, self.parse_immediate(self.tokens[0].value[1:])))
-                except:
-                    operands.append(DataItem(OperandType.LABEL, self.tokens[0].value[1:]))
-            elif self.tokens[0].type == TokenType.STRING:
-                operands.append(DataItem(OperandType.STRING, self.tokens[0].value))
-            else:
+            try:
+                operands.append(self.parse_operand(self.current_token()))
+            except Exception:
                 break
-            self.tokens.pop(0)
-
         return Data(operands)
 
+    def parse_expression(self) -> int:
+        if self.current_token().value == "!":
+            self.consume_token()
+            return not self.parse_expression()
+        elif self.current_token().value == "-":
+            self.consume_token()
+            return self.parse_rhs(0, -self.parse_immediate())
+        elif self.current_token().value == "(":
+            self.consume_token()
+            expression = self.parse_expression()
+            if not self.current_token().value == ")":
+                raise Exception("Expected closing bracket, found {}".format(
+                    self.current_token().value))
+            self.consume_token()
+            return self.parse_rhs(0, expression)
+        elif self.current_token().type == TokenType.IMMEDIATE:
+            lhs = self.parse_immediate()
+            return self.parse_rhs(0, lhs)
+        else:
+            raise Exception("Invalid expression: {}".format(
+                self.current_token().value))
+
+    def parse_rhs(self, prev_precedence, lhs):
+        while True:
+            operator = self.current_token().value
+            if precedence(operator) < prev_precedence:
+                return lhs
+
+            self.consume_token()
+            rhs = self.parse_immediate()
+            next_operator = self.current_token().value
+            if precedence(operator) < precedence(next_operator):
+                rhs = self.parse_rhs(
+                    precedence(self.current_token().value), rhs)
+            match operator:
+                case "+":
+                    lhs = lhs + rhs
+                case "-":
+                    lhs =  lhs - rhs
+                case "*":
+                    lhs =  lhs * rhs
+                case _:
+                    raise Exception("Invalid operator: {}".format(operator))
+
     def parse_interrupt(self) -> Interrupt:
-        self.tokens.pop(0)
-        if not self.tokens[0].type == TokenType.IMMEDIATE:
-            raise Exception("Expected Immediate after 'interrupt'")
-        number = self.parse_immediate(self.tokens[0].value)
-        self.tokens.pop(0)
+        self.consume_token()
+        number = self.parse_expression()
+        self.consume_token()
         if number >= 0 and number <= 16:
             return Interrupt(number)
-        else: 
+        else:
             raise Exception("Interrupt number must be between 0 and 16")
 
     def parse(self, contents) -> List[Instruction | Label | Macro | Data]:
         self.instructions: List[Instruction] = []
-        self.stream: List [Label | Instruction | Macro] = []
+        self.stream: List[Label | Instruction | Macro] = []
         self.tokens = self.lexer.lex(contents)
         while len(self.tokens) > 0:
-            match self.tokens[0].type:
+            match self.current_token().type:
                 case TokenType.WORD:
                     instruction = self.parse_instruction()
-                    self.stream.append(instruction) 
+                    self.stream.append(instruction)
                 case TokenType.LABEL:
-                    self.stream.append(Label(self.tokens[0].value[:-1]))
-                    self.tokens.pop(0)
+                    self.stream.append(Label(self.current_token().value[:-1]))
+                    self.consume_token()
                 case TokenType.START_MACRO:
                     macro: Macro = self.parse_macro()
                     self.stream.append(macro)
@@ -213,12 +292,15 @@ class Parser:
                     interrupt: Interrupt = self.parse_interrupt()
                     self.stream.append(interrupt)
                 case TokenType.INCLUDE:
-                    self.tokens.pop(0)
-                    if not self.tokens[0].type == TokenType.WORD:
-                        raise Exception("Expected filename after include, got: {}".format(self.tokens[0].value))
+                    self.consume_token()
+                    if not self.current_token().type == TokenType.WORD:
+                        raise Exception("Expected filename after include, got: {}".format(
+                            self.current_token().value))
                     parser = Parser()
-                    self.stream += parser.parse_file(self.tokens[0].value)
-                    self.tokens.pop(0)
+                    self.stream += parser.parse_file(
+                        self.current_token().value)
+                    self.consume_token()
                 case _:
-                    raise Exception("Invalid token: {}".format(self.tokens[0].value))
+                    raise Exception("Invalid token: {}".format(
+                        self.current_token().value))
         return self.stream
